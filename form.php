5c12374fbf12dccdf4225ab561400bbada1c96a4
lam nhat <zznhatphilong@gmail.com>
2023-12-09 00:16:23 +0700

    second commit

--- form.php
+++ form.php
@@ -4,26 +4,22 @@ function validate_message($message)
 {
     // function to check if message is correct (must have at least 10 caracters (after trimming))
     $message = trim($message);
-    if (strlen($message) < 10) return "Message must be at least 10 characters long";
-    return "";
+    if (strlen($message) < 10) return false;
+    return true;
 }
 
 function validate_username($username)
 {
     // function to check if username is correct (must be alphanumeric => Use the function 'ctype_alnum()')
     $username = trim($username);
-    if (empty($username)) return "Please enter a username";
-    if (!ctype_alnum($username)) return "Username should contains only letters and numbers" ;
-    return "";
+    return ctype_alnum($username);
 }
 
 function validate_email($email)
 {
     // function to check if email is correct (must contain '@') 
     $email = trim($email);
-    if (empty($email)) return "Please enter a email";
-    if (strstr($email,'@') == false) return "email must contain '@'";
-    return "";
+    return strpos($email,'@');
 }
 
 
@@ -37,8 +33,6 @@ $message = "";
 
 $form_valid = false;
 
-$isPostMethod = false;
-
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Here is the list of error messages that can be displayed:
@@ -49,15 +43,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // "Please enter an email"
     // "email must contain '@'"
 
-    $isPostMethod = true;
-
     $username = htmlspecialchars($_POST['username']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    $user_error = validate_username($username);
-    $email_error = validate_email($email);
-    $message_error = validate_message($message);
+    if (empty($email)) $email_error = "Please enter a email";
+    elseif (!validate_email($email)) $email_error = "email must contain '@'";
+
+    if (empty($username)) $user_error = "Please enter a username";
+    elseif (!validate_username($username)) $user_error = "Username should contains only letters and numbers";
+
+    if (!validate_message($message)) $message_error = "Message must be at least 10 characters long";
 
     if (!isset($_POST['terms'])) $terms_error = "You must accept the Terms of Service";
 
@@ -65,24 +61,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 
 ?>
-
 <form action="#" method="post">
     <div class="row mb-3 mt-3">
         <div class="col">
-            <input type="text" class="form-control" placeholder="Enter Name" name="username" value="<?php if($isPostMethod) echo $_POST['username']?>">
+            <input type="text" class="form-control" placeholder="Enter Name" name="username" value="<?php if(isset($_POST['username'])) echo $_POST['username']?>">
             <small class="form-text text-danger"> <?php echo $user_error; ?></small>
         </div>
         <div class="col">
-            <input type="text" class="form-control" placeholder="Enter email" name="email" value="<?php if($isPostMethod) echo $_POST['email']?>">
+            <input type="text" class="form-control" placeholder="Enter email" name="email" value="<?php if(isset($_POST['email'])) echo $_POST['email']?>">
             <small class="form-text text-danger"> <?php echo $email_error; ?></small>
         </div>
     </div>
     <div class="mb-3">
-        <textarea name="message" placeholder="Enter message" class="form-control"><?php if($isPostMethod) echo $_POST['message']?></textarea>
+        <textarea name="message" placeholder="Enter message" class="form-control"><?php if(isset($_POST['message'])) echo $_POST['message']?></textarea>
         <small class="form-text text-danger"> <?php echo $message_error; ?></small>
     </div>
     <div class="mb-3">
-        <input type="checkbox" class="form-control-check" name="terms" id="terms" value="terms"> <label for="terms">I accept the Terms of Service</label>
+        <input type="checkbox" class="form-control-check" name="terms" id="terms" <?php if(isset($_POST['terms'])) echo "checked"?> value="terms"> <label for="terms">I accept the Terms of Service</label>
         <small class="form-text text-danger"> <?php echo $terms_error; ?></small>
     </div>
     <div class="d-grid">
